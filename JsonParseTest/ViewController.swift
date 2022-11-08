f66436064d1a7b91028ef25058f05e309c877f93
Pavel <80887778+chulep@users.noreply.github.com>
2022-11-08 19:41:46 +0300

    Initial Commit

---
+++ JsonParseTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonParseTest
+//
+//  Created by Pavel Schulepov on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
