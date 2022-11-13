9a7ea47d67fab5eaf3c1acd66d5464b25b90b87c
Pavel <80887778+chulep@users.noreply.github.com>
2022-11-13 18:00:19 +0300

    DetailViewController #3
    end

--- JsonParseTest/DetailModule/DetailViewController.swift
+++ JsonParseTest/DetailModule/DetailViewController.swift
@@ -10,37 +10,64 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var networkManager = NetworkManager()
-    var result: Results!
-    var imageView = UIImageView()
+    
+    var result: Results! {
+        didSet {
+            nameLebel.text = "Name: " + (result.user?.username ?? "")
+            dateLabel.text = "Date: " + result.created_at
+            descriptionLabel.text = "Description: " + (result.description ?? "")
+        }
+    }
+    
+    lazy var imageView: UIImageView = {
+        $0.contentMode = .scaleAspectFit
+        return $0
+    }(UIImageView())
+    
     var nameLebel = UILabel()
     var dateLabel = UILabel()
     var descriptionLabel = UILabel()
-    var descriptionStackView = UIStackView()
-    var activityIndicator = UIActivityIndicatorView(style: .medium)
+    
+    lazy var descriptionStackView: UIStackView = {
+        for i in [dateLabel, nameLebel, descriptionLabel] {
+            $0.addArrangedSubview(i)
+        }
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
+        $0.axis = .vertical
+        $0.backgroundColor = #colorLiteral(red: 0.8627251983, green: 0.8735858798, blue: 0.8733949065, alpha: 1)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 7, bottom: 0, right: 7)
+        $0.isLayoutMarginsRelativeArrangement = true
+        return $0
+    }(UIStackView())
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        $0.style = .medium
+        return $0
+    }(UIActivityIndicatorView())
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        createUI()
+        view.backgroundColor = .white
+        for i in [imageView, descriptionStackView, activityIndicator] {
+            view.addSubview(i)
+        }
+        setupNavBar()
+        createLayout()
         addHideTapGestureRecognizer()
-        setInfo()
         setImage()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         descriptionStackView.layer.cornerRadius = descriptionStackView.bounds.height / 10
-        descriptionLabel.clipsToBounds = true
+        descriptionStackView.clipsToBounds = true
     }
     
     //MARK: - Set Data
-    func setInfo() {
-        nameLebel.text = "Name: " + (result.user?.username ?? "")
-        dateLabel.text = "Date: " + result.created_at
-        descriptionLabel.text = "Description: " + (result.description ?? "")
-    }
-    
-    func setImage() {
+    private func setImage() {
         activityIndicator.startAnimating()
         networkManager.getPic(url: result.urls.full) { data in
             guard let data = data else { return }
@@ -54,34 +81,24 @@ class DetailViewController: UIViewController {
     }
     
     //MARK: - UI
-    func createUI() {
-        view.backgroundColor = .white
-        imageView.contentMode = .scaleAspectFit
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(cancel))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: #selector(saveToFavorites))
-        navigationController?.navigationBar.tintColor = .systemGray
-        
-        descriptionStackView = UIStackView(arrangedSubviews: [dateLabel, nameLebel, descriptionLabel])
-        descriptionStackView.alignment = .fill
-        descriptionStackView.distribution = .fillEqually
-        descriptionStackView.axis = .vertical
-        descriptionStackView.backgroundColor = #colorLiteral(red: 0.8627251983, green: 0.8735858798, blue: 0.8733949065, alpha: 1)
-        descriptionStackView.translatesAutoresizingMaskIntoConstraints = false
-        descriptionStackView.layoutMargins = UIEdgeInsets(top: 0, left: 7, bottom: 0, right: 7)
-        descriptionStackView.isLayoutMarginsRelativeArrangement = true
-        
-        for i in [imageView, descriptionStackView, activityIndicator] {
-            view.addSubview(i)
-        }
-        
+    private func createLayout() {
         imageView.frame = view.bounds
         activityIndicator.center = imageView.center
+        
         NSLayoutConstraint.activate([
-            descriptionStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 7),
-            descriptionStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -7),
+            descriptionStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            descriptionStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
             descriptionStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             descriptionStackView.heightAnchor.constraint(equalTo: descriptionStackView.widthAnchor, multiplier: 1/3)
         ])
+    }
+    
+    private func setupNavBar() {
+        navigationController?.navigationBar.tintColor = .systemGray
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(cancelDetail))
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: #selector(saveToFavorites)),
+            UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(sharePhoto))]
     }
     
     //MARK: - Description Hide Methods
@@ -104,12 +121,17 @@ class DetailViewController: UIViewController {
         }
     }
     
-    //MARK: - Cancel Method
-    @objc func cancel() {
+    //MARK: - objc Method
+    @objc func cancelDetail() {
         dismiss(animated: true)
     }
     
     @objc func saveToFavorites() {
         
+    }
+    
+    @objc func sharePhoto() {
+        let share = UIActivityViewController(activityItems: [result.urls.full], applicationActivities: nil)
+        present(share, animated: true)
     }
 }
